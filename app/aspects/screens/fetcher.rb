71faee672f9b3bcb996c63f2379267ba631b678b
Kayra <kayraucklnc@gmail.com>
2026-08-27 01:58:58 +0300

    wip: remove the playlist and design model
    Checkpoint before merging origin/main. References are still being cleaned up.

--- app/aspects/screens/fetcher.rb
+++ app/aspects/screens/fetcher.rb
@@ -10,51 +10,14 @@ module Dither
         include Deps[
           "aspects.screens.interrupts.sleep",
           "aspects.scenes.resolver",
-          "aspects.scenes.publisher",
-          rule_repository: "repositories.rule",
-          playlist_repository: "repositories.playlist",
-          playlist_item_repository: "repositories.playlist_item"
+          "aspects.scenes.publisher"
         ]
         include Dry::Monads[:result]
 
         def call device
           return sleep.call device if device.asleep?
-          return from_rules device if rules? device
 
-          from_playlist device
-        end
-
-        private
-
-        # Rules are the model going forward; playlists remain for devices that
-        # have not been moved across yet, so upgrading does not blank a panel.
-        def rules?(device) = rule_repository.for_device(device.id).any?
-
-        def from_rules device
-          resolver.call(device)
-                  .bind { |decision| publisher.call decision.scene, device: }
-        end
-
-        def from_playlist device
-          find_playlist(device.playlist_id).bind { |playlist| find_current_item playlist }
-                                           .fmap(&:screen)
-        end
-
-        def find_playlist id
-          playlist = playlist_repository.find id
-
-          return Success playlist if playlist
-
-          Failure "Unable to fetch screen. Can't find playlist with ID: #{id.inspect}."
-        end
-
-        def find_current_item playlist
-          id = playlist.current_item_id
-          item = playlist_item_repository.find id
-
-          return Success item if item
-
-          Failure "Unable to fetch screen. Can't find current playlist item with ID: #{id.inspect}."
+          resolver.call(device).bind { |decision| publisher.call decision.scene, device: }
         end
       end
     end
--- app/aspects/screens/rotator.rb
+++ app/aspects/screens/rotator.rb
@@ -5,48 +5,23 @@ require "dry/monads"
 module Dither
   module Aspects
     module Screens
-      # Updates a device's current playlist item by rotating to next screen.
+      # Answers the screen a device should be showing, and renders it.
+      #
+      # Named for what it used to do - rotate through a playlist. It now asks
+      # the rules which scene applies and renders that, which is the same
+      # question with a better answer.
       class Rotator
         include Deps[
           "aspects.screens.interrupts.sleep",
-          playlist_repository: "repositories.playlist",
-          item_repository: "repositories.playlist_item"
+          "aspects.scenes.resolver",
+          "aspects.scenes.publisher"
         ]
         include Dry::Monads[:result]
 
         def call device
-          if device.asleep?
-            sleep.call device
-          else
-            find_playlist(device.playlist_id).fmap { |playlist| advance_current_item playlist }
-                                             .bind { |item| obtain_screen item }
-          end
-        end
+          return sleep.call device if device.asleep?
 
-        private
-
-        def find_playlist id
-          playlist = playlist_repository.find id
-
-          return Success playlist if playlist
-
-          Failure "Unable to obtain next screen. Can't find playlist with ID: #{id.inspect}."
-        end
-
-        # :reek:FeatureEnvy
-        def advance_current_item playlist
-          return playlist.current_item if playlist.manual?
-
-          item_repository.subsequent(
-            playlist_id: playlist.id,
-            position: playlist.current_item_position
-          ).tap { |item| playlist_repository.update_current_item playlist, item }
-        end
-
-        def obtain_screen item
-          return Success item.screen if item
-
-          Failure "Unable to obtain next screen. Playlist has no items."
+          resolver.call(device).bind { |decision| publisher.call decision.scene, device: }
         end
       end
     end
